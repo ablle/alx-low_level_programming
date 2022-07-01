96f99bff31a79bf9e150e426c3f248268dcc47d2
Ajayi Blessing Tope <ablezzing93@gmail.com>
2022-07-01 02:35:33 -0700

    positive anything is beeter

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,22 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
-int main(void)
+#include <stdio.h>
+/**
+ * main > assign a random number to the variable n each time it is executed
+ * ouput >The output of the program  willbase on a giving condition
+ */
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if(n>0)
 {
-		int n;
-
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
-					/* your code goes there */
-					return (0);
+	printf("%d is positive\n",n);
+}
+ifelse(n<0)
+{
+	printf("%d is negative\n",n);
+}
+else(n=0){
+	printf("%d is zero\n",n);
+}
+return (0);

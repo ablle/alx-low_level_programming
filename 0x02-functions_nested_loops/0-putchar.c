4ee1b57cfc80ce3abe00e432fc862758c5e4bd3a
Ajayi Blessing Tope <ablezzing93@gmail.com>
2022-07-05 13:41:01 -0700

    a program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
  * main - Print "_putchar" followed by a new line.
- * Description : You are not allowed to include standard libraries.
- * Return : 0 on success
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
  */
 int main(void)
 {
-	/*declear a char veriable*/
-	char printme[] = "_putchar";
+	char holberton[] = "_putchar";
 	int i = 0;
 
-	while (printme[i] != '\0')
+	while (holberton[i] != '\0')
 	{
-		_putchar(printme[i]);
+		_putchar(holberton[i]);
 		i++;
 	}
 	_putchar('\n');

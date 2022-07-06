3b4ca62231dafb735cd0eae9b9f40e7fbda7d5c2
AJAYI BLESSING TOPE <107250889+ablle@users.noreply.github.com>
2022-07-06 16:07:27 +0100

    Update 1-alphabet.c
     function that prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_alphabet - Print alphabet in lowercase using only `_putchar`
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+        while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+TTermwhile (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
